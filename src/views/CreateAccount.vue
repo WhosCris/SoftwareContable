2781aebd470ce2bb99b0c23d37b36e3756939724
Sekain555 <antonio.rms30@gmail.com>
2025-02-28 22:06:28 -0300

    victor-create project

---
+++ src/views/CreateAccount.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-container>
+        <v-row align="center" justify="start">
+            <v-col cols="auto">
+                <v-img width="64" max-height="64" src="/src/assets/contabilidad.png" contain></v-img>
+            </v-col>
+            <v-col>
+                <span class="text-h3 font-weight-bold text-primary" style="font-family: 'Space Grotesk'">Software
+                    Contable</span>
+            </v-col>
+        </v-row>
+
+        <v-row class="mt-12" justify="space-around">
+            <v-col cols="6">
+                <v-row>
+                    <span class="text-h4 font-weight-bold text-primary" style="font-family: 'Space Grotesk'">Crea una
+                        cuenta nueva</span>
+                </v-row>
+                <v-row class="mt-6">
+                    <span class="text-h6 font-stretch-100% text-primary" style="font-family: 'Inter'">Ingresa tus datos
+                        para crear una cuenta
+                        nueva</span>
+                </v-row>
+            </v-col>
+            <v-col cols="5">
+                <v-card color="card" class="pa-6" elevation="12">
+                    <v-card-title class="text-h4 font-weight-bold text-center text-primary">Crear cuenta</v-card-title>
+
+                    <v-card-text>
+                        <v-text-field label="Nombre completo" required clearable variant="outlined" class="mt-3">
+
+                        </v-text-field>
+                        <v-text-field label="Correo electrónico" required clearable variant="outlined" class="mt-3">
+
+                        </v-text-field>
+                        <v-text-field label="Contraseña" required clearable variant="outlined" class="mt-3">
+
+                        </v-text-field>
+
+                        <v-btn :disabled="!isValid" block color="buttonprimary" class="mt-4">
+                            Crear cuenta
+                        </v-btn>
+                    </v-card-text>
+
+                    <v-card-actions class="justify-center">
+                        <v-btn variant="text" color="buttonprimary" @click="gotoLogin">¿Ya tienes
+                            una cuenta? Inicia sesión</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    methods: {
+        gotoLogin() {
+            this.$router.push('/');
+        }
+    },
+}
+</script>
+
+<style scoped>
+.fill-height {
+    height: 100vh;
+}
+</style>
---
+++ src/views/MainMenu.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="3">
+                <v-card color="card" elevation="12">
+                    <v-container>
+                        <v-row align="center" justify="center" class="ma-2">
+                            <v-avatar size="128">
+                                <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+                            </v-avatar>
+                        </v-row>
+                        <v-row align="center" justify="center" class="ma-2">
+                            <span class="text-h5 font-weight-bold text-primary"
+                                style="font-family: 'Space Grotesk'">Nombre Apellido</span>
+                        </v-row>
+                        <v-row align="center" justify="center" class="mx-2 mt-2">
+                            <v-img height="24" src="/src/assets/icons/correo-electronico-24px.png"></v-img>
+                        </v-row>
+                        <v-row align="center" justify="center" class="mx-2 mb-2">
+                            <span class="text-body-1 font-stretch-100% text-primary"
+                                style="font-family: 'Inter'">correo@electronico.cl</span>
+                        </v-row>
+                        <v-row align="center" justify="center" class="mx-2 mt-2">
+                            <v-img height="24" src="/src/assets/icons/correo-electronico-24px.png"></v-img>
+                        </v-row>
+                        <v-row align="center" justify="center" class="mx-2 mb-2">
+                            <span class="text-body-1 font-stretch-100% text-primary"
+                                style="font-family: 'Inter'">correo@electronico.cl</span>
+                        </v-row>
+                        <v-row align="center" justify="center" class="mt-4 mb-1 mx-2">
+                            <v-btn color="buttonprimary" append-icon="mdi-account-edit">Editar perfil</v-btn>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+            <v-col cols="9">
+                <v-card color="card" elevation="12">
+                    <v-container>
+                        <v-row class="ma-1">
+                            <v-text-field label="Buscar empresa" variant="outlined"
+                                prepend-inner-icon="mdi-magnify"></v-text-field>
+                        </v-row>
+                        <v-divider></v-divider>
+                        <v-row align="center" justify="center" class="ma-2">
+                            <span class="text-h4 ma-2" style="font-family: 'Space Grotesk'">Dashboard Empresas</span>
+                        </v-row>
+                        <v-row align="start" justify="start" class="ma-2">
+                            <v-col cols="4">
+                                <v-card elevation="12">
+                                    <v-container>
+                                        <v-card-title class="text-center">NUEVA EMPRESA</v-card-title>
+                                        <v-row justify="center" class="ma-2">
+                                            <v-img max-width="128" src="/src/assets/icons/anadir-empresa-128px.png"
+                                                contain></v-img>
+                                        </v-row>
+                                    </v-container>
+                                </v-card>
+                            </v-col>
+                            <v-col cols="4">
+                                <v-card elevation="12">
+                                    <v-container>
+                                        <v-row justify="space-between">
+                                            <v-btn color="buttonprimary" icon="mdi-star-outline" variant="text"></v-btn>
+                                            <v-btn color="buttonprimary" icon="mdi-dots-vertical"
+                                                variant="text"></v-btn>
+                                        </v-row>
+                                        <v-card-title class="text-center">CIVO Tech</v-card-title>
+                                        <v-row justify="center" class="ma-2">
+                                            <v-img max-width="128" src="/src/assets/icons/anadir-empresa-128px.png"
+                                                contain></v-img>
+                                        </v-row>
+                                        <v-row justify="space-evenly">
+                                            <v-dialog max-width="340">
+                                                <template v-slot:activator="{ props: activatorProps }">
+                                                    <v-btn color="buttonprimary" icon="mdi-eye" variant="text"
+                                                        v-bind="activatorProps"></v-btn>
+                                                </template>
+                                                <template v-slot:default="{ isActive }">
+                                                    <v-card title="Ref Activator"
+                                                        text="Cuando usas el slot activator, el diálogo se activará correctamente.">
+                                                        <template v-slot:actions>
+                                                            <v-btn class="ml-auto" @click="isActive.value = false">Cerrar</v-btn>
+                                                        </template>
+                                                    </v-card>
+                                                </template>
+                                            </v-dialog>
+                                            <v-btn color="buttonprimary" icon="mdi-notebook-edit"
+                                                variant="text"></v-btn>
+                                        </v-row>
+                                    </v-container>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<style scoped>
+.fill-height {
+    height: 100vh;
+}
+</style>
